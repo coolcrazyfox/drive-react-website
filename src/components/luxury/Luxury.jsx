2498abeb791429a76f058533ac9911acbb159c38
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-10 21:05:34 +0300

    change luxury

--- src/components/luxury/Luxury.jsx
+++ src/components/luxury/Luxury.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import styles from './Luxury.module.css'
+import Card from "../find/Card";
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 const Luxury = () => {
   return (
@@ -15,30 +22,84 @@ const Luxury = () => {
       </div>
 
       <div className={styles.container}>
-          <div className={styles.card}>
-              <img src="https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGx1eHVyeSUyMGNhcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
-              <div className={styles.content}>
-                  <h3>Porsche</h3>
-              </div>
-          </div>
-          <div className={styles.card}>
-              <img src="https://images.unsplash.com/photo-1563720360172-67b8f3dce741?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHJhbmdlJTIwcm92ZXJ8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
-              <div className={styles.content}>
-                  <h3>Range Rover</h3>
-              </div>
-          </div>
-          <div className={styles.card}>
-            <img src="https://images.pexels.com/photos/3764984/pexels-photo-3764984.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-            <div className={styles.content}>
-                <h3>Rolls Royce</h3>
-            </div>
-        </div>
-        <div className={styles.card}>
-            <img src="https://images.pexels.com/photos/6894429/pexels-photo-6894429.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-            <div className={styles.content}>
-                <h3>Maserati</h3>
-            </div>
-        </div>
+          <Swiper
+              // install Swiper modules
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              spaceBetween={10}
+              slidesPerView={5}
+              navigation
+              breakpoints={{
+                  // when window width is >= 340px
+                  340: {
+                      width: 200,
+                      slidesPerView: 1,
+                  },
+                  // when window width is >= 768px
+                  768: {
+                      width: 768,
+                      slidesPerView: 3,
+                  },
+                  // when window width is >= 1040px
+                  1040: {
+                      width: 1040,
+                      slidesPerView: 4,
+                  },
+              }}
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log('slide change')}
+          >
+              <SwiperSlide>
+                  <Card
+                      image='https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/1-aston-martin-victor-2021-fd-hero-front.jpg?itok=q6UHOJtM'
+                      make='Aston Martin'
+                      href='https://www.astonmartin.com/en/'
+                  />
+              </SwiperSlide>
+              <SwiperSlide>
+                  <Card
+                      image='https://motor.ru/imgs/2019/09/02/08/3541317/21c2375b048d884226a5b8ea5dce4c5ffd388a14.jpg'
+                      make='Bugatti'
+                      href='https://www.audiusa.com/us/web/en.html'
+                  />
+              </SwiperSlide>
+              <SwiperSlide>
+                  <Card
+                      image='https://s1.cdn.autoevolution.com/images/news/gallery/beefed-up-ferrari-sf90-hides-novitec-goodies-behind-satin-black-and-red-attire_1.jpg'
+                      make='Ferrari'
+                      href='https://www.audiusa.com/us/web/en.html'
+                  />
+              </SwiperSlide>
+              <SwiperSlide>
+                  <Card
+                      image='https://www.motorbiscuit.com/wp-content/uploads/2022/02/Fully-loaded-new-gray-2022-Jaguar-F-TYPE-R-driving-on-a-mountain-highway.jpg'
+                      make='Jaguar'
+                      href='https://www.audiusa.com/us/web/en.html'
+                  />
+              </SwiperSlide>
+              <SwiperSlide>
+                  <Card
+                      image='https://maserati.scene7.com/is/image/maserati/maserati/international/Brand/Alfieri/2016-06-01/141510M.JPG?$1400x2000$&fit=constrain'
+                      make='Maserati'
+                      href='https://www.audiusa.com/us/web/en.html'
+                  />
+              </SwiperSlide>
+              <SwiperSlide>
+                  <Card
+                      image='https://motor.ru/thumb/0x750/filters:quality(75)/imgs/2021/08/27/09/4853180/a5f0a8fad63703f91c936e580709f29ab587689c.jpg'
+                      make='Range  Rover'
+                      href='https://www.audiusa.com/us/web/en.html'
+                  />
+              </SwiperSlide>
+              <SwiperSlide>
+                  <Card
+                      image='https://s2.glbimg.com/w2yM2-e5icts2CEJOVpkXG9owHo=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2021/A/B/cUIEpTQASqltmZYGMGIg/rolls-royce-ghost-black-badge-2021-warehouse-2.jpg'
+                      make='Rolls Royce'
+                      href='https://www.audiusa.com/us/web/en.html'
+                  />
+              </SwiperSlide>
+          </Swiper>
         </div>
     </div>
   )
