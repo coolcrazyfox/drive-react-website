9de2b1e542073ba7bb28d9e57ef12638695715c4
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-14 23:47:28 +0300

    create index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import {HashRouter} from "react-router-dom";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HashRouter>
