3721d32208ef518daa6b97923a0d721be3af4669
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-15 00:08:59 +0300

    change Signup.jsx

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,37 +1,42 @@
 import React from 'react';
 import s from './Signup.module.css'
+import Navbar from "../navbar/Navbar";
 
 const Signup = () => {
     return (
-        <section>
-            <div className={s.color} ></div>
-            <div className={s.color}></div>
-            <div className={s.color}></div>
-            <div className={s.box}>
-                <div className={s.square} ></div>
-                <div className={s.square} ></div>
-                <div className={s.square} ></div>
-                <div className={s.square} ></div>
-                <div className={s.square} ></div>
-                <div className={s.container}>
-                    <div className={s.form}>
-                        <h2 data-text="&nbsp;Sign Up Form">&nbsp;Sign Up Form</h2>
-                        <form >
-                            <div className={s.inputBox}>
-                                <input type="text" placeholder={"Username"}/>
-                            </div>
-                            <div className={s.inputBox}>
-                                <input type="password" placeholder={"Password"}/>
-                            </div>
-                            <div className={s.inputBox}>
-                                <input type="submit" value={"Login"}/>
-                            </div>
-                        </form>
+        <>
+            <Navbar/>
+            <section>
+                <div className={s.color} ></div>
+                <div className={s.color}></div>
+                <div className={s.color}></div>
+                <div className={s.box}>
+                    <div className={s.square} ></div>
+                    <div className={s.square} ></div>
+                    <div className={s.square} ></div>
+                    <div className={s.square} ></div>
+                    <div className={s.square} ></div>
+                    <div className={s.container}>
+                        <div className={s.form}>
+                            <h2 data-text="&nbsp;Sign Up Form">&nbsp;Sign Up Form</h2>
+                            <form >
+                                <div className={s.inputBox}>
+                                    <input type="text" placeholder={"Username"}/>
+                                </div>
+                                <div className={s.inputBox}>
+                                    <input type="password" placeholder={"Password"}/>
+                                </div>
+                                <div className={s.inputBox}>
+                                    <input type="submit" value={"Login"}/>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </section>
+            </section>
+        </>
+
     );
 };
 
