79b8a49f8c9496d0f017a3e75a7d3cefdeab9822
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-18 22:48:08 +0300

    change Search.css

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
-
+import s from './Search.module.css'
 const Search = () => {
     return (
         <div>
-            Search
+            <ul className={s.navigation}>
+                <li>
+                    <a href="#">
+                        <span className={s.icon} ></span>
+                        <span className={s.text}>Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span className={s.icon}></span>
+                        <span className={s.text}>Profile</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span className={s.icon}></span>
+                        <span className={s.text}>Messages</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span className={s.icon}></span>
+                        <span className={s.text}>Photos</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span className={s.icon}></span>
+                        <span className={s.text}>Settings</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 };
