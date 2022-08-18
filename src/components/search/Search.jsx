4425272786aae0e1e08e2b04b97181f21d831e96
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-19 00:42:43 +0300

    change Search.css

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import home from './home-outline.svg'
-import s from './Search.module.css'
+import home from './home-outline.svg';
+import prof from './person-circle-outline.svg';
+import chat from './chatbubble-ellipses-outline.svg';
+import photo from './camera-outline.svg';
+import setting from './settings-outline.svg';
+
+import s from './Search.module.css';
 const Search = () => {
     return (
         <div className={s.main_container}>
@@ -14,25 +19,25 @@ const Search = () => {
                     </li>
                     <li className={s.active}>
                         <a href="#">
-                            <span className={s.icon}></span>
+                            <span className={s.icon}  style={{backgroundImage:`url(${prof})`}}></span>
                             <span className={s.text}>Profile</span>
                         </a>
                     </li>
                     <li className={s.active}>
                         <a href="#">
-                            <span className={s.icon}></span>
+                            <span className={s.icon} style={{backgroundImage:`url(${chat})`}}></span>
                             <span className={s.text}>Messages</span>
                         </a>
                     </li>
                     <li className={s.active}>
                         <a href="#">
-                            <span className={s.icon}></span>
+                            <span className={s.icon} style={{backgroundImage:`url(${photo})`}}></span>
                             <span className={s.text}>Photos</span>
                         </a>
                     </li>
                     <li className={s.active}>
                         <a href="#">
-                            <span className={s.icon}></span>
+                            <span className={s.icon} style={{backgroundImage:`url(${setting})`}}></span>
                             <span className={s.text}>Settings</span>
                         </a>
                     </li>
