6954075f6edcc028e1765c0fadc15fd96c193206
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-19 00:30:52 +0300

    change Search.css

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import home from './home-outline.svg'
 import s from './Search.module.css'
 const Search = () => {
     return (
@@ -7,31 +8,31 @@ const Search = () => {
                 <ul className={s.navigation}>
                     <li className={s.active}>
                         <a href="#">
-                            <span className={`s.icon ${s.icon_home}}`} ></span>
+                            <span className={s.icon} style={{backgroundImage:`url(${home})`}}></span>
                             <span className={s.text}>Home</span>
                         </a>
                     </li>
                     <li className={s.active}>
                         <a href="#">
-                            <span className={s.icon_pro}></span>
+                            <span className={s.icon}></span>
                             <span className={s.text}>Profile</span>
                         </a>
                     </li>
                     <li className={s.active}>
                         <a href="#">
-                            <span className={s.icon_message}></span>
+                            <span className={s.icon}></span>
                             <span className={s.text}>Messages</span>
                         </a>
                     </li>
                     <li className={s.active}>
                         <a href="#">
-                            <span className={s.icon_photo}></span>
+                            <span className={s.icon}></span>
                             <span className={s.text}>Photos</span>
                         </a>
                     </li>
                     <li className={s.active}>
                         <a href="#">
-                            <span className={s.icon_set}></span>
+                            <span className={s.icon}></span>
                             <span className={s.text}>Settings</span>
                         </a>
                     </li>
