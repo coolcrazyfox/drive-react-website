f2a2f4ef472582b55cc53ff71afa72f33aa91305
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-18 23:03:24 +0300

    change Search.css

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -6,31 +6,31 @@ const Search = () => {
             <ul className={s.navigation}>
                 <li>
                     <a href="#">
-                        <span className={s.icon} ></span>
+                        <span className={s.icon_home} ></span>
                         <span className={s.text}>Home</span>
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <span className={s.icon}></span>
+                        <span className={s.icon_pro}></span>
                         <span className={s.text}>Profile</span>
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <span className={s.icon}></span>
+                        <span className={s.icon_message}></span>
                         <span className={s.text}>Messages</span>
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <span className={s.icon}></span>
+                        <span className={s.icon_photo}></span>
                         <span className={s.text}>Photos</span>
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <span className={s.icon}></span>
+                        <span className={s.icon_set}></span>
                         <span className={s.text}>Settings</span>
                     </a>
                 </li>
