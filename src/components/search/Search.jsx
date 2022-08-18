4f639b169b5da7bad7a74a8c5a9efeecbd806c2b
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-18 23:56:24 +0300

    change Search.css

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,39 +2,42 @@ import React from 'react';
 import s from './Search.module.css'
 const Search = () => {
     return (
-        <div>
-            <ul className={s.navigation}>
-                <li>
-                    <a href="#">
-                        <span className={s.icon_home} ></span>
-                        <span className={s.text}>Home</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span className={s.icon_pro}></span>
-                        <span className={s.text}>Profile</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span className={s.icon_message}></span>
-                        <span className={s.text}>Messages</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span className={s.icon_photo}></span>
-                        <span className={s.text}>Photos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span className={s.icon_set}></span>
-                        <span className={s.text}>Settings</span>
-                    </a>
-                </li>
-            </ul>
+        <div className={s.main_container}>
+            <div className={s.box}>
+                <ul className={s.navigation}>
+                    <li className={s.active}>
+                        <a href="#">
+                            <span className={`s.icon ${s.icon_home}}`} ></span>
+                            <span className={s.text}>Home</span>
+                        </a>
+                    </li>
+                    <li className={s.active}>
+                        <a href="#">
+                            <span className={s.icon_pro}></span>
+                            <span className={s.text}>Profile</span>
+                        </a>
+                    </li>
+                    <li className={s.active}>
+                        <a href="#">
+                            <span className={s.icon_message}></span>
+                            <span className={s.text}>Messages</span>
+                        </a>
+                    </li>
+                    <li className={s.active}>
+                        <a href="#">
+                            <span className={s.icon_photo}></span>
+                            <span className={s.text}>Photos</span>
+                        </a>
+                    </li>
+                    <li className={s.active}>
+                        <a href="#">
+                            <span className={s.icon_set}></span>
+                            <span className={s.text}>Settings</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
         </div>
     );
 };
